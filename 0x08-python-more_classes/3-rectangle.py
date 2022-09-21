eba4c2a0da29f06523bbfa7953ab5ebcc8079fbe
Ras Haile <105021283+Rashaileguevara@users.noreply.github.com>
2022-09-21 22:38:04 +0300

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,75 +1,59 @@
 #!/usr/bin/python3
 """
-    Module contains of a single class
+Defines a Rectangle Class
 """
 
 
 class Rectangle:
-    """Defines a reactangle"""
-
+    """Represents a Rectangle"""
     def __init__(self, width=0, height=0):
-        """Initializing of instance data"""
-        if not isinstance(width, (int, float)):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if not isinstance(height, (int, float)):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
-
-    def __str__(self):
-        """Prints rectangle using `#` character"""
-        string = []
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        for i in range(self.__height):
-            for j in range(self.__width):
-                string.append("#")
-            if i < (self.__height - 1):
-                string.append("\n")
-        return "".join(string)
-
-    def __repr__(self):
-        """Prints string representation of Rectangle"""
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+        """Defines  a Rectangle"""
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """Retrieves the value of `width`"""
+        """setter of private instance width attribute"""
         return self.__width
-
-    @property
-    def height(self):
-        """Retrieves the value of `height`"""
-        return self.__height
 
     @width.setter
     def width(self, value):
-        """Sets the value of atribute `width` to new value"""
-        if not isinstance(value, (int, float)):
+        """setter of private instance width attribute"""
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """setter of private instance height attribute"""
+        return self.__height
+
     @height.setter
     def height(self, value):
-        """Sets the value of atribute `height` to new value"""
-        if not isinstance(value, (int, float)):
+        """setter of private instance height attribute"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """Calculates the area of a reactangle"""
-        return (self.__width * self.__height)
+        """Return the area of a square"""
+        return self.__width * self.__height
 
     def perimeter(self):
-        """Calculates the perimeter of a reactangle"""
-        if self.__width == 0 or self.__height == 0:
+        """Return the perimeters of a square"""
+        if self.__width is 0 or self.__height is 0:
             return 0
-        return ((self.__width + self.__height) * 2)
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return a square"""
+        string = ""
+        string = ""
+        if self.__width != 0 and self.__height != 0:
+            string += "\n".join("#" * self.__width
+                                for j in range(self.__height))
+        return string
