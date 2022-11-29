dbfa178a346340a3f491ed5fc28e786ca51d84db
Ras Haile <105021283+Rashaileguevara@users.noreply.github.com>
2022-11-30 00:59:36 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
-exports.dict = {
-  89: 1,
-  90: 2,
-  91: 1,
-  92: 3,
-  93: 1,
-  94: 2
-};
+const { dict } = require('./101-data');
+
+const newd = {};
+let list = [];
+for (const [key, value] of Object.entries(dict)) {
+  if (!(value in newd)) {
+    list = [];
+  } else {
+    list = newd[value];
+  }
+  list.push(key);
+  newd[value] = list;
+}
+console.log(newd);
